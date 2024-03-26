7a4021a7a13bde10db15759b51519b5772bbc5f1
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-03-26 15:58:41 -0400

    Insertado logo en navbar y modificado el color de background de la pagina

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,11 @@
-
+import logo from "../assets/logo.svg"
 export const NavBar = () => {
     return(
         <>
             <nav className="bg-green-500">
-                <div className="p-3">
-                    <h1 className="text-xl text-center">Messenger API</h1>
+                <div className="flex items-center justify-center p-3">
+                    <img className="w-7 h-6 text-cyan-400" src={logo} alt="Logo Messenger" />
+                    <h1 className="text-xl">Messenger API</h1>
                 </div>
             </nav>
         </>
