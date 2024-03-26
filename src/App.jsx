02bab27055a3e146bcf819c9b0a3a33f5952ff37
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-03-26 10:52:22 -0400

    Diseño preliminar del navbar implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import { NavBar } from "./components/navbar"
 
 function App() {
 
   return (
     <>
-      <h1 className='text-orange-700 text-3xl'>Hola Mundo con tailwind</h1>
+      <NavBar/>
     </>
   )
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+
+export const NavBar = () => {
+    return(
+        <>
+            <nav className="bg-green-500">
+                <div className="p-3">
+                    <h1 className="text-xl text-center">Messenger API</h1>
+                </div>
+            </nav>
+        </>
+    )
+}
