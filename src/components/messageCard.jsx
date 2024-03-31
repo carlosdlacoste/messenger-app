c282d7e3640535992e5ed779284781fc7b07ffed
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-03-31 15:27:46 -0400

    Mejorado eslilo de letra del header de los cards

--- src/components/messageCard.jsx
+++ src/components/messageCard.jsx
@@ -6,9 +6,9 @@ export const MessageCard = (props) => {
         <>
             <div className="bg-slate-800 grid gap-y-4 shadow-xl shadow-black hover:bg-slate-600 hover:cursor-pointer">
                 <div className="flex flex-col justify-between items-center bg-slate-500 border-solid border-2 border-red-300 p-1 h-max">
-                    <h3 className="text-2xl text-white break-all">{props.senderNumber}</h3>
+                    <h3 className="text-2xl text-white font-extrabold break-all">{props.senderNumber}</h3>
                     <img className="w-7 h-6" src={arrow} alt="" />
-                    <h3 className="text-2xl text-white break-all">{props.receivedNumber}</h3>
+                    <h3 className="text-2xl text-white font-extrabold break-all">{props.receivedNumber}</h3>
                 </div>
                 <div className="text-xl text-justify space-y-4 p-3 h-80">
                     <p className="text-green-300">Sender: <span className="text-white font-extrabold uppercase">{props.senderName}</span></p>
